77ea0f12955ca3a81563fb9a4f83e9f11e1eeaf8
ops zak <ops@zak.app>
2022-01-29 15:42:17 -0300

    Add lastpass wrapper

---
+++ api/clinet.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"context"
+
+	"github.com/ansd/lastpass-go"
+	"github.com/jltorresm/otpgo"
+)
+
+// Secret describes a Lastpass Secret object.
+type Secret struct {
+	Group           string `json:"group"`
+	ID              string `json:"id"`
+	LastModifiedGmt string `json:"last_modified_gmt"`
+	LastTouch       string `json:"last_touch"`
+	Name            string `json:"name"`
+	Note            string `json:"note"`
+	Password        string `json:"password"`
+	Share           string `json:"share"`
+	URL             string `json:"url"`
+	Username        string `json:"username"`
+}
+
+func (secret *Secret) fill(account *lastpass.Account) {
+	secret.Group = account.Group
+	secret.ID = account.ID
+	secret.LastModifiedGmt = account.LastModifiedGMT
+	secret.LastTouch = account.LastTouch
+	secret.Name = account.Name
+	secret.Note = account.Notes
+	secret.Password = account.Password
+	secret.Share = account.Share
+	secret.URL = account.URL
+	secret.Username = account.Username
+
+}
+
+// Client is a Lastpass client wrapper.
+type Client struct {
+	Username string
+	Password string
+	Totp     string
+}
+
+func (c *Client) login() (*lastpass.Client, error) {
+
+	totp := otpgo.TOTP{
+		Key: c.Totp,
+	}
+
+	token, error := totp.Generate()
+	if error != nil {
+		return nil, error
+	}
+
+	client, error := lastpass.NewClient(context.Background(), c.Username, c.Password, lastpass.WithOneTimePassword(token))
+	if error != nil {
+		return nil, error
+	}
+
+	return client, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/apecnascimento
 
 go 1.17
+
+require github.com/ansd/lastpass-go v0.3.1
+
+require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+
+require (
+	github.com/jltorresm/otpgo v0.3.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+)
