d4de911376f57d53162d329677b16b5f1973fe15
ops zak <ops@zak.app>
2022-01-29 16:07:32 -0300

    Add Terraform data source secret

--- go.mod
+++ go.mod
@@ -4,9 +4,35 @@ go 1.17
 
 require github.com/ansd/lastpass-go v0.3.1
 
-require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+require (
+	github.com/agext/levenshtein v1.2.2 // indirect
+	github.com/apparentlymart/go-textseg v1.0.0 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/hashicorp/go-version v1.3.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.3.0 // indirect
+	github.com/hashicorp/terraform-plugin-go v0.5.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	github.com/zclconf/go-cty v1.9.1 // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
 
 require (
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.10.1
 	github.com/jltorresm/otpgo v0.3.0
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 )
---
+++ lastpass/data_source_secret.go
@@ -0,0 +1,103 @@
+package lastpass
+
+import (
+	"context"
+	"errors"
+	"strconv"
+
+	"github.com/apecnascimento/api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// DataSourceSecret describes our lastpass secret data source
+func DataSourceSecret() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: DataSourceSecretRead,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"username": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"password": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+			"last_modified_gmt": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"last_touch": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"group": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"note": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func fillResourceData(d *schema.ResourceData, laspassSecret api.Secret) {
+	d.SetId(laspassSecret.ID)
+	d.Set("name", laspassSecret.Name)
+	d.Set("username", laspassSecret.Username)
+	d.Set("password", laspassSecret.Password)
+	d.Set("last_modified_gmt", laspassSecret.LastModifiedGmt)
+	d.Set("last_touch", laspassSecret.LastTouch)
+	d.Set("group", laspassSecret.Group)
+	d.Set("url", laspassSecret.URL)
+	d.Set("note", laspassSecret.Note)
+}
+
+// DataSourceSecretRead reads resource from upstream/lastpass
+func DataSourceSecretRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*api.Client)
+	var diags diag.Diagnostics
+
+	id := d.Get("id").(string)
+	name := d.Get("name").(string)
+
+	if id != "" {
+
+		if _, err := strconv.Atoi(id); err != nil {
+			err := errors.New("Not a valid Lastpass ID")
+			return diag.FromErr(err)
+		}
+		secret, err := client.GetByID(id)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		fillResourceData(d, secret)
+
+	} else if name != "" {
+		secret, err := client.GetByName(name)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		fillResourceData(d, secret)
+	} else {
+		d.SetId("")
+	}
+
+	return diags
+}
